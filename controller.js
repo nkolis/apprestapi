474a8e30ac251ff2d05f142a7e8a81629af0ed77
setiawan <nurkholis010@gmail.com>
2020-12-24 09:39:28 +0700

    berhasil menampilkan data

--- controller.js
+++ controller.js
@@ -7,3 +7,15 @@ var connection = require('./koneksi');
 exports.index = function (req,res){
     response.ok("Aplikasi REST API ku berjalan!",res);
 }
+
+
+//menampilkan semua data mahasiswa
+exports.tampilSemuaMahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds){
+        if(error){
+            console.log(error);
+        } else {
+            response.ok(rows,res);
+        }
+    })
+}
