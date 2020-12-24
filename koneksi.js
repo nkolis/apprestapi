f71711130acd10cfef9a395292a5d83c113e4833
setiawan <nurkholis010@gmail.com>
2020-12-24 09:16:22 +0700

    update all

--- koneksi.js
+++ koneksi.js
@@ -5,7 +5,7 @@ const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'dbrestapi'
+    database: 'restapi'
 });
 
 conn.connect( (err) => {
